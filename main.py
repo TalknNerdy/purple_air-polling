0cc144f542281e5e449d8d9da16204b44589096a
Kevin Coleman <kevin.coleman@sparkstart.io>
2019-12-28 08:47:18 -0500

    Fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import app.ifttt as ifttt
 import app.purple_air as purple_air
 import config
+import platform
+print('version', platform.python_version())
 
 config_values = config.load_config()
 data = purple_air.get_data()
