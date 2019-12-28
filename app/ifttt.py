a06545af7c1414ef3d87d3684eafc56451f8f4de
Kevin Coleman <kevin.coleman@sparkstart.io>
2019-12-28 08:58:54 -0500

    Fix

--- app/ifttt.py
+++ app/ifttt.py
@@ -10,5 +10,5 @@ def send_alert(id, value):
   event_name = os.environ['ifttt_EVENT_NAME']
   key = os.environ['ifttt_KEY']
   url = f'https://maker.ifttt.com/trigger/{event_name}/with/key/{key}'
-  response = requests.post(url, headers=headers, data=json.dumps({'high_id': id, 'high_value': value}))
+  response = requests.post(url, headers=headers, data=json.dumps({'value1': id, 'value2': value}))
   print("sent alert", id, value)
